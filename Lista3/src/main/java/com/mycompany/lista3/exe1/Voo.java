bf88d94afae231f7086a2e76b54f3bccf41682a7
João Victor <78619731+joaovictorperes@users.noreply.github.com>
2022-09-16 20:53:44 -0300

    Add files via upload

---
+++ Lista3/src/main/java/com/mycompany/lista3/exe1/Voo.java
@@ -0,0 +1,47 @@
+
+package com.mycompany.lista3.exe1;
+
+public class Voo {
+    private int id;
+    private String origem, destino;
+    
+    public Voo() {
+        
+    }
+
+    public Voo(int id, String origem, String destino) {
+        this.id = id;
+        this.origem = origem;
+        this.destino = destino;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOrigem() {
+        return origem;
+    }
+
+    public void setOrigem(String origem) {
+        this.origem = origem;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+    
+    @Override
+    public String toString() {
+        return "Voo{" + "id=" + id + ", origem=" + origem + ", destino=" + destino + '}';
+    } 
+    
+}
